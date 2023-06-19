c49924bc2684710baed69d16c5b22f60efd4afd3
Jay Cheng <326091867@qq.com>
2023-06-19 21:55:41 +0800

    Story: 本 commit 提交下正确的用例，本次需要注意的点是，poolTest 没有挂载到 prefab 上，导致排查了一会。

--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -11,8 +11,14 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+
+    private void Update()
+    {
         if (Input.GetKeyDown(KeyCode.A))
         {
+            Debug.Log("begin test.");
             PoolManager.Instance.GetGameObject<TestPool>(Cube);
         }
     }
